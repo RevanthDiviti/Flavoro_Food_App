53fe0a06fe4a572fdc77b2a9756409c3c36072c0
RevanthDiviti <145599691+RevanthDiviti@users.noreply.github.com>
2025-07-28 03:15:26 +0530

    .

--- Flavoro/src/Components/FoodCard.jsx
+++ Flavoro/src/Components/FoodCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { addToCart, removeFromCart } from "../redux/slices/CartSlice";
 
 const FoodCard = ({ id, name, price, desc, img, rating }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className="font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2">
       <img
@@ -13,14 +17,17 @@ const FoodCard = ({ id, name, price, desc, img, rating }) => {
         <h2>{name}</h2>
         <p className="text-green-500">₹{price}</p>
       </div>
-      <p className="text-sm font-normal ">
-        {desc.slice(0,50)}...
-      </p>
+      <p className="text-sm font-normal ">{desc.slice(0, 50)}...</p>
       <div className="flex justify-between">
         <span className="flex items-center justify-center">
           <AiFillStar className="mr-1 text-yellow-400" /> {rating}
         </span>
-        <button className="p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg ">
+        <button
+          onClick={() => {
+            dispatch(addToCart({ id, name, price, img, rating, qty: 1 }));
+          }}
+          className="p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg cursor-pointer "
+        >
           Add to Cart
         </button>
       </div>
